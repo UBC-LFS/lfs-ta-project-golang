6ed3e407b1d1a0419c4107bda4fefe24ce49c866
DonaldKLee <Donald3010@outlook.com>
2023-09-29 12:54:04 -0700

    testing api calls

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// When run without any options
+// 		Returns csv list of courses, instructor names, instructor emails
+// Script should have options to,
+// 		list available terms from API
+// 		supply a term to get assignment for that term
+// 		Supply a list of unique instructor names and email in csv format
 
 func pullTerms() {
+	URL := "https://sat.api.ubc.ca/academic-exp/v2"
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	fmt.Println(resp)
 }
 
 func main() {
